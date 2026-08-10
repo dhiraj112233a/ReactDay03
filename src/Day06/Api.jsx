a7c16edf3c7b563f52251e3d669637e4c72e7f8c
dhiraj112233a <dhirajdalvi2005@gmail.com>
2026-08-10 09:24:38 +0530

    File Modified

--- src/Day06/Api.jsx
+++ src/Day06/Api.jsx
@@ -8,30 +8,34 @@ const Main = () => {
           setStudents(std.data)
     })
   return (
-    <div className='container p-2'>
-         <h1>Students List</h1>
-         <table className='table table-bordered border border-4'>
-              <tr>
-                  <th className='border border-4 p-3'>Post Id</th>
-                  <th className='border border-4 p-3'>ID</th>
-                  <th className='border border-4 p-3'>Name</th>
-                  <th className='border border-4 p-3'>Email</th>
-                  <th className='border border-4 p-3'>Body</th>
-              </tr>
-               {
-                students.map((e)=>(
-                   <tr>
-                     <>
-                      <td className='border border-2 p-2'>{e.postId}</td>
-                      <td className='border border-2 p-2'>{e.id}</td>
-                      <td className='border border-2 p-2'>{e.name}</td>
-                      <td className='border border-2 p-2'>{e.email}</td>
-                      <td className='border border-2 p-2'>{e.body}</td>
-                    </>
-                   </tr>
-                ))
-               }
-         </table>
+    <div className='container p-3 d-flex flex-column align-items-center'>
+         <h1 className='text-center mb-4'>Comments List</h1>
+         <div className='table-responsive w-100'>
+           <table className='table table-striped table-hover table-bordered align-middle shadow-sm rounded overflow-hidden mx-auto'>
+                <thead className='table-dark'>
+                  <tr>
+                      <th className='p-3'>Post Id</th>
+                      <th className='p-3'>ID</th>
+                      <th className='p-3'>Name</th>
+                      <th className='p-3'>Email</th>
+                      <th className='p-3'>Body</th>
+                  </tr>
+                </thead>
+                <tbody>
+                 {
+                  students.map((e)=>(
+                     <tr key={e.id}>
+                        <td className='p-2'>{e.postId}</td>
+                        <td className='p-2'>{e.id}</td>
+                        <td className='p-2'>{e.name}</td>
+                        <td className='p-2'>{e.email}</td>
+                        <td className='p-2'>{e.body}</td>
+                     </tr>
+                  ))
+                 }
+                </tbody>
+           </table>
+         </div>
     </div>
   )
 }
